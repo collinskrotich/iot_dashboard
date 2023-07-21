b3de5449302654c5e4578d6983c8149f7990ba5e
DianaMunene <dmunene@safaricom.co.ke>
2023-07-21 14:21:45 +0300

    changed side bar bg color and icons size

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -12,53 +12,53 @@ import {MdForest} from 'react-icons/md';
 const Sidebar = ({ children }) => {
   return (
     <div className='flex'>
-        <div className='fixed w-90 h-screen p-3 bg-black border-r-[1px] flex flex-col justify-between'>
+        <div className='fixed w-240 h-screen p-4 bg-neutral-800 border-r-[1px] flex flex-col justify-between'>
             <div className='flex flex-col items-center'>
                 <div>
-                    <img className='pb-6'src='/../saf-logo.png' alt='safaricom-logo' width='125' height='160'>                                   
+                    <img className='pb-10'src='/../saf-logo.png' alt='safaricom-logo' width='125' height='200'>                                   
                     </img>                  
                     </div>  
             
                 <Link href ='/'>
-                    <div className='bg-green-600 text-white p-3 rounded-lg inline-block'>
-                        <GiIceCube size = '40'/>
+                    <div className='bg-green-600 text-white p-1 rounded-lg inline-block'>
+                        <GiIceCube size = '45'/>
                         IOT CUBE
                     </div>
                 </Link>
-                <span className='border-b-[2px] border-gray-200 w-full py-2'></span>
+                    <span className='border-b-[1px] border-gray-200 w-full py-2'></span>
                 <Link href ='/' >
-                    <div className='bg-green-600 text-white p-4 mt-8 rounded-lg inline-block' >
-                        <RxDashboard size = '40'/>
+                    <div className='bg-green-600 text-white p-3 mt-10 rounded-lg inline-block' >
+                        <RxDashboard size = '35'/>
                         HOME
                     </div>
                 </Link>
-
+                <Link href ='/maps' >
+                    <div className='bg-green-600 text-white p-4 mt-10 rounded-lg inline-block' >
+                        <TbMapPins size = '35'/>
+                        MAP
+                    </div>
+                </Link>
                 <Link href ='/readings' >
-                    <div className='bg-green-600 text-white p-3 mt-10 rounded-lg inline-block' >
-                        <FaTemperatureLow size = '40'/>
+                    <div className='bg-green-600 text-white p-2 mt-10 rounded-lg inline-block' >
+                        <FaTemperatureLow size = '30'/>
                         READINGS
                     </div>
                 </Link>
 
                 <Link href ='/users' >
-                    <div className='bg-green-600 text-white p-3 mt-10 rounded-lg inline-block' >
-                        <MdForest size = '40'/>
-                        REGIONS
+                    <div className='bg-green-600 text-white p-2 mt-10 rounded-lg inline-block' >
+                        <MdForest size = '30'/>
+                        REGIONS   
                     </div>
                 </Link>
 
 
 
-                <Link href ='/maps' >
-                    <div className='bg-green-600 text-white p-3 mt-10 rounded-lg inline-block' >
-                        <TbMapPins size = '40'/>
-                        MAP
-                    </div>
-                </Link>
+               
 
                 <Link href ='/' >
-                    <div className='bg-green-600 text-white p-3 mt-10 rounded-lg inline-block' >
-                        <FiSettings size = '60'/>
+                    <div className='bg-green-600 text-white p-5 mt-10 rounded-lg inline-block' >
+                        <FiSettings size = '30'/>
                     </div>
                 </Link>
 
