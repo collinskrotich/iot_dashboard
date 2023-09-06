efac4d09eb2a26eb10d8173b14df905c9da02580
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-09-06 17:01:15 +0300

    camel case on user name

--- components/Header.jsx
+++ components/Header.jsx
@@ -21,9 +21,9 @@ const Header = () => {
     <Authenticator>
       {({ user }) => (
     <div className='flex justify-between px-20 pt-4'>
-        <h2 className='text-xl'>Air Quality Environmental Monitoring Dashboard</h2>
+        <h2 className='text-xl'>Air Quality Dashboard</h2>
         <div className="flex flex-row">
-        <div className='pt-1 pr-2'><h2>Welcome back,  {toCamelCase(user.username) }</h2></div>
+        <div className='pt-1 pr-2'><h2>Welcome back, {user.username.charAt(0).toUpperCase() + user.username.slice(1)}</h2></div>
         <div className='relative'>
           <img class="w-10 h-10 rounded-full" src="/motorcyclist.png" alt=""/> 
           <span class="absolute top-0 left-8 transform -translate-y-1/2 w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span> 
